32acaac86950a92ad57da62a997781900f31296d
Garry Ledwith <garryledwith@gmail.com>
2025-07-16 02:58:12 +0100

    Initial commit

---
+++ list.py
@@ -0,0 +1,18 @@
+# LIsts in Python
+mylist1 = ['banana', 'apple', 'orange']
+
+print(mylist1)
+
+mylist2 = list()
+print(mylist2)
+
+mylist3 = [5, True, 'hello']
+print(mylist3)
+
+item = mylist1[0-1]
+print(item)
+
+# loop through a list (for loop)
+print("Looping through mylist1:")
+for i in mylist1:
+    print(i)
