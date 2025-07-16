45cd3f49f93d70747b53b8c11b1353e95cf766ca
Garry Ledwith <garryledwith@gmail.com>
2025-07-16 03:17:42 +0100

    practicing tuples

---
+++ tuples.py
@@ -0,0 +1,7 @@
+# Tuple is a collection datatype that is ordered and immutable.
+# It can hold a collection of items, which can be of different data types.
+# Tuples are defined using parentheses () instead of square brackets [] like lists.
+# Tuples are often used to group related data together.
+# Example of a tuple containing strings
+mytuple1 = ('banana', 'apple', 'orange')
+print(mytuple1[0])  # Accessing the first element ('banana')
