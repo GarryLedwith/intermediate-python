ac017c80e507cd0d015d4085c92c4e5354614ff9
Garry Ledwith <garryledwith@gmail.com>
2025-07-16 04:11:14 +0100

    deleting, conditions and try, except

--- dicts.py
+++ dicts.py
@@ -9,3 +9,25 @@ print(value)  # Output: Alice
 
 mydict2 = dict()  # Creating an empty dictionary
 print(mydict2)
+
+mydict1['email'] = 'garry@gmail.com'  # Adding a new key-value pair
+print(mydict1)
+
+# deleting items from a dictionary
+del mydict1['age'] # Deleting a key-value pair by key
+print(mydict1)
+
+mydict1.pop('city')  # Removing a key-value pair and returning its value
+print(mydict1)
+
+# conditionals in dictionaries
+if 'name' in mydict1:  # Checking if a key exists
+    print("Name is in mydict1")
+else:
+    print("Name is not in mydict1")
+
+# try except for key access
+try:
+    value = mydict1['age']  # Trying to access a key that may not exist
+except KeyError:
+    print("Key 'age' does not exist in mydict1")    
