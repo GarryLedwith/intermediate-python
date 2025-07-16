f83eb55f4b2858ee0663e8d2e667bab18204d2af
Garry Ledwith <garryledwith@gmail.com>
2025-07-16 03:25:05 +0100

    practicing tuples

--- tuples.py
+++ tuples.py
@@ -5,3 +5,18 @@
 # Example of a tuple containing strings
 mytuple1 = ('banana', 'apple', 'orange')
 print(mytuple1[0])  # Accessing the first element ('banana')
+print(type(mytuple1))  # Output: <class 'tuple'>
+
+mytuple2 = ("Hello") # This is not a tuple, it's just a string
+print(type(mytuple2))  # Output: <class 'str'> (a comma is needed to create a tuple with one item)
+mytuple3 = ("Hello",)  # This is a tuple with one item
+print(type(mytuple3))  # Output: <class 'tuple'>
+
+# built in tuple function
+mytuple3 = tuple([1, 2, 3])  # Convert a list to a tuple
+print(mytuple3)  # Output: (1, 2, 3)
+
+# iterating through a tuple
+print("Looping through mytuple1:")
+for i in mytuple1:
+    print(i)
