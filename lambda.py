a9a113c667d3166c28508ea00f114a9a0eb926e3
Garry Ledwith <garryledwith@gmail.com>
2025-07-18 07:06:15 +0100

    updating lambda section

--- lambda.py
+++ lambda.py
@@ -2,6 +2,7 @@
 # They can take any number of arguments but can only have one expression.
 # Lambda functions are often used for short, throwaway functions that are not reused elsewhere.
 # Lambda syntax: lambda arguments: expression
+# typically used in higher-order functions like map, filter, and reduce.
 # Example of a simple lambda function
 add = lambda x, y: x + y  # A lambda function that adds two numbers
 print(add(2, 3))  # Output: 5
